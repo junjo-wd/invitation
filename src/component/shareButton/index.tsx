10639b03477288e89f7ba031a48408bd65ecb520
MM <nmmn@MMui-MacBookPro.local>
2025-09-18 20:29:31 +0900

    qlfem fdn

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -58,7 +58,12 @@ export const ShareButton = () => {
                     window.location.protocol +
                     "//" +
                     window.location.host +
-                    process.env.PUBLIC_URL,                  
+                    process.env.PUBLIC_URL,
+                  webUrl:
+                    window.location.protocol +
+                    "//" +
+                    window.location.host +
+                    process.env.PUBLIC_URL,
                 },
               },
             ],
