51ac5f95ac55d71f64fe228adc3cfe2d568d6af4
MM <nmmn@MMui-MacBookPro.local>
2025-11-27 13:28:40 +0900

    “Depoly2511_A6”

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -14,64 +14,7 @@ export const ShareButton = () => {
   const kakao = useKakao()
   return (
     <LazyDiv className="footer share-button">
-      <button
-        className="ktalk-share"
-        onClick={() => {
-          if (!kakao) {
-            return
-          }
-
-          kakao.Share.sendDefault({
-            objectType: "location",
-            address: SHARE_ADDRESS,
-            addressTitle: SHARE_ADDRESS_TITLE,
-            content: {
-              title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description:
-                WEDDING_DATE.format("YYYY년 MMMM D일 dddd A h시") +
-                "\n" +
-                LOCATION,
-              imageUrl:
-                window.location.protocol +
-                "//" +
-                window.location.host +
-                process.env.PUBLIC_URL +
-                "/preview_image.png",
-              link: {
-                mobileWebUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  process.env.PUBLIC_URL,
-                webUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  process.env.PUBLIC_URL,
-              },
-            },
-            buttons: [
-              {
-                title: "초대장 보기",
-                link: {
-                  mobileWebUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    process.env.PUBLIC_URL,
-                  webUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    process.env.PUBLIC_URL,
-                },
-              },
-            ],
-          })
-        }}
-      >
-
-      <button
+            <button
         className="ktalk-share"
         onClick={() => {
           navigator.clipboard.writeText("https://junjo-wd.github.io/invitation/")
@@ -84,8 +27,6 @@ export const ShareButton = () => {
         }}
       >
         <img src={UrlIcon} alt="url-icon" /> URL 주소 복사하기
-      </button>
-        {/* <img src={ktalkIcon} alt="ktalk-icon" /> 카카오톡으로 공유하기 */}
       </button>
     </LazyDiv>
   )
